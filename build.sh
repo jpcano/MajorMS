28f1e6a38d8e62ded2660e2abee1ff7f44405ae6
Jesus Cano <jcanovel@gmail.com>
2025-03-23 02:41:06 +0000

    Use conan presets

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-#rm -rf build/
-conan install . --output-folder=build --build=missing
-cmake . -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -B build/
-cmake --build build/ --parallel $(nproc --all)
+rm -rf build/
+
+#conan install . --output-folder=build --build=missing
+#cmake . -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -B build/
+#cmake --build build/ --parallel $(nproc --all)
+
+conan install . --output-folder=build
+#conan install . --output-folder=build --settings=build_type=Debug
+cmake --preset conan-release
+#cmake --preset conan-debug
+cmake --build --preset conan-release --parallel $(nproc --all)
+
