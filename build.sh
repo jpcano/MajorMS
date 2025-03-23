bfcf1af21131c0edfa653f478f7339e3106738d5
Jesus Cano <jcanovel@gmail.com>
2025-03-23 14:27:14 +0000

    Add cpack

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 rm -rf build/
 rm -rf dist/
+rm -rf pack/
 
 #conan install . --output-folder=build --build=missing
 #cmake . -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -B build/
@@ -12,4 +13,7 @@ cmake --preset conan-release
 cmake --build --preset conan-release --parallel $(nproc --all)
 
 cmake --install build --prefix dist
+mkdir pack
+#(cd pack && cpack --config ../build/CPackConfig.cmake && cpack --config ../build/CPackSourceConfig.cmake)
+(cd pack && cpack --config ../build/CPackConfig.cmake)
 
